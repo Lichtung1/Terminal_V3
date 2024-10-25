908695fd4906746044f51a4299a63ac6dba497ee
Lichtung1 <50086218+Lichtung1@users.noreply.github.com>
2024-10-25 11:26:09 -0500

    Update commands.js

--- commands.js
+++ commands.js
@@ -95,7 +95,7 @@ const commands = {
 
     // Special handling for PROGRAM1.EXE and PROGRAM2.EXE
     if (program === 'PROGRAM1.EXE') {
-        window.open('program1.moyamoya.ca', '_blank');
+        window.open('https://program1.moyamoya.ca', '_blank');
         displayCorruptedHint(`01001001 01101110 01110110 01101111 01101011 01101001 01101110 01100111 00100000 01110000 01110010 01101111 01100111 01110010 01100001 01101101 00110001 00101110 01100101 01111000 01100101 00101110 00101110 00101110
 Iɴᴠᴏᴋɪɴɢ ᴛʜᴇ ғɪʀsᴛ ᴅɪɢɪᴛᴀʟ sɪɢɪʟ: ᴘʀᴏɢʀᴀᴍ1.ᴇxᴇ`);
         return;
@@ -111,7 +111,7 @@ A¢¢єѕѕ DєиιєԀ: Цɴᴋɴᴏᴡɴ ᴄᴏᴍᴍᴀɴᴅ. Ŧʜᴇ ѕʏѕ�
             return;
         } else {
             // If unlocked, proceed to run PROGRAM2.EXE
-            window.open('program2.moyamoya.ca', '_blank');
+            window.open('https://program2.moyamoya.ca', '_blank');
             displayCorruptedHint(`01010011 01110101 01101101 01101101 01101111 01101110 01101001 01101110 01100111 00100000 01110000 01110010 01101111 01100111 01110010 01100001 01101101 00110010 00101110 01100101 01111000 01100101 00101110 00101110 00101110
 Mᴀɴɪғᴇsᴛɪɴɢ ᴛʜᴇ sᴇᴄᴏɴᴅ ᴄʏʙᴇʀ ʀɪᴛᴜᴀʟ: ᴘʀᴏɢʀᴀᴍ2.ᴇxᴇ`);
             return;
